e331e19c072162f3fadfd4902f472ea95a84baff
groovytron <julien.mpoy@gmail.com>
2016-10-23 15:33:27 +0000

    Applied fixes from StyleCI

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
 use TwigBridge\Facade\Twig;
 
 class LoginController extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         return Twig::render('login');
     }
 
-    public function login() {
-        $this->validate(request(),[
+    public function login()
+    {
+        $this->validate(request(), [
             'email' => 'required|email|max:255',
             'password' => 'required|max:255',
         ]);
