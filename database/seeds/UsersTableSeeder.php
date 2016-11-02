df08540b0cc24a22a028bc428850e49f30dd99c5
Julien M'Poy <julien.mpoy@gmail.com>
2016-11-02 13:26:29 +0100

    User seeder with one simple user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => bcrypt('test')
+        ]);
+    }
+}
