376f5d62deed3f0a1b21c56d342887175dad7bd9
Julien M'Poy <julien.mpoy@gmail.com>
2016-11-02 17:04:33 +0100

    Rename product type model following camelCase

--- app/ProductType.php
+++ app/ProductType.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product_type extends Model
+class ProductType extends Model
 {
     /**
      * The table associated with the model.
