7248cf2da21092e542c9b0d9814624518b9e0b02
Sylvain <sylvain.ramseyer@gmail.com>
2016-11-16 14:42:17 +0100

    database link error soleving and add php debug bar

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(15);
+        $products = Product::with('type')->get();
+        //dd($products);
         return view('inventory', compact('products'));
     }
 
