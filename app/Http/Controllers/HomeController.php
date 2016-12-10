f373366948019d27c70758fbb93e65b2246d148b
Julien M'Poy <julien.mpoy@gmail.com>
2016-12-10 21:50:22 +0100

    Add a index page for the inventory actions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
         $links = [
            [
                 'title' => 'Inventory',
-                'link' => '',
+                'link' => route('inventory'),
                 'glyphicon' => 'glyphicon-inbox',
             ],
             [
@@ -38,6 +38,51 @@ class HomeController extends Controller
                 'title' => 'Customers',
                 'link' => '',
                 'glyphicon' => 'glyphicon-user',
+            ],
+        ];
+
+        return view('home', compact('title', 'jumbotronMessage', 'links'));
+    }
+    /**
+     * Show the inventory dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function inventoryIndex()
+    {
+        $title = 'Inventory';
+        $jumbotronMessage = 'Please choose an action in the above menu.';
+        $links = [
+           [
+                'title' => 'On hands inventory',
+                'link' => route('product.index', [
+                    'inventorySlug' => 'on-hands'
+                ]),
+                'glyphicon' => 'glyphicon-inbox',
+            ],
+            [
+                'title' => 'Consignment inventory',
+                'link' => route('product.index', [
+                    'inventorySlug' => 'consignment'
+                ]),
+                'glyphicon' => 'glyphicon-inbox',
+            ],
+            [
+                'title' => 'Sales',
+                'link' => route('product.index', [
+                    'inventorySlug' => 'sales'
+                ]),
+                'glyphicon' => 'glyphicon-inbox',
+            ],
+            [
+                'title' => 'Move to packship',
+                'link' => '',
+                'glyphicon' => 'glyphicon-inbox',
+            ],
+            [
+                'title' => 'Move to a remote inventory',
+                'link' => '',
+                'glyphicon' => 'glyphicon-inbox',
             ],
         ];
 
